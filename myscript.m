a8a70d5c9e90601f6d00f73af6440912f196ef28
SherifZ <60384279+SherifAI-enthusiat@users.noreply.github.com>
2023-01-15 14:47:50 +0000

    Added file paths to allow os paths

--- myscript.m
+++ myscript.m
@@ -1,15 +1,14 @@
 function [outputn] = myscript(x)
 %     This is a counter to create individual folders for results and
 %     extract by Par-pool
-    load('MatlabOutput\\expData.mat');
     count =1; key = true;
-    scalarM = 100.*ones(size(expData));
     while key==true
-        cmd1 = sprintf('runDir\\workspace_%d',count);
+        ff = fullfile('MatlabOutput',{'expData.mat';sprintf('output_%d',count)});
+        cmd1 = fullfile('runDir', sprintf('workspace_%d',count));
         if exist(cmd1)==7
             count = count + 1;
         else
-            cmd1 = sprintf('MatlabOutput\\output_%d.mat',count);
+%             cmd1 = fullfile('runDir',sprintf('workspace_%d',count));
             key = false;
         end
     end
@@ -17,7 +16,9 @@ function [outputn] = myscript(x)
     formatSpec = 'lstestv2_parallel.py %d %d %d %d %d %d %d %d %d %d'; %% This is where I can change bits.
     cmd = sprintf(formatSpec,x(1),x(2),x(3),x(4),x(5),x(6),x(7),x(8),x(9),count);
     pyrunfile(cmd);
-    tmp = load(cmd1, 'dat');
+    load(string(ff(1)));
+    scalarM = 100.*ones(size(expData));
+    tmp = load(string(ff(2)), 'dat');
     outputn = errorfunc(tmp.dat,expData,scalarM,1);
         %     if exist(cmd1)==2
         %         delete(cmd1);
