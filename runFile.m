5c2716d38d41685e303a9bfdd3dac78cf6c03438
Sherif Zantiba <60384279+SherifAI-enthusiat@users.noreply.github.com>
2024-06-06 07:33:47 +0100

    Last edit on test case version

--- runFile.m
+++ runFile.m
@@ -5,7 +5,7 @@ Obj = myFunctions();
 Obj.path = "D:\Sherif_CT_Download\github\Abaqus_FE_Optim\MatlabOutput";
 x0 =[20,50,1.4583]; % Here I am only interested in Ep,Ef and Gpf [20,20,100,0.3,0.2,0.2,4.7115,1.4583,1.4583]
 lb =[.01,1,1]; % This is the lower bound 
-ub =[20,250,30]; % This is the lower bound 
+ub =[20,250,30]; % This is the upper bound 
 % %% Optimisation using particle swarm from scratch
 options = optimoptions('particleswarm','SwarmSize',7,'HybridFcn', @fmincon,'InertiaRange',[1.5,0.5]);
 options.UseParallel = true;
